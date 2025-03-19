c847e14d115d5ea1d30a791b8da3c8e81a9b6b0b
k1lluazzz <148640620+k1lluazzz@users.noreply.github.com>
2025-03-19 16:10:16 +0700

    test2

--- src/routes/carRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getCars, createCar } = require("../controllers/carController");
-
-const router = express.Router();
-router.get("/", getCars);
-router.post("/", createCar);
-
-module.exports = router;
